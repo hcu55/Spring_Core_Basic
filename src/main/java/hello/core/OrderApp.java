0938321f74a8b296e0e36f9e31552eda74144fb3
ChanUi <hcu55@naver.com>
2023-10-02 23:27:02 +0900

    :sparkles: 주문과 할인 도메인 실행과 테스트 구현

---
+++ src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,26 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.oreder.Order;
+import hello.core.oreder.OrderService;
+import hello.core.oreder.OrderServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.creteOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+        System.out.println("order = " + order.calculatePrice());
+    }
+}
