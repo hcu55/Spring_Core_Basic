ed1b3deb21a76ac15d350eec00642f788099637d
ChanUi <hcu55@naver.com>
2024-08-26 23:04:39 +0900

    :sparkles: 탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages = "hello.core.member",           // 해당 패키지부터 하위 패키지 컴포넌트 스캔
+//        basePackageClasses = AutoAppConfig.class,     // 지정한 클래스의 패키지를 탐색 시작 위치로 지정
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
