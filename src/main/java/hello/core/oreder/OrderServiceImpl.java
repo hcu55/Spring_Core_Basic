89823a6bd94345c702c878943abd5783df813a23
ChanUi <hcu55@naver.com>
2024-09-01 14:01:54 +0900

    :sparkles: 롬복과 최신 트랜드

--- src/main/java/hello/core/oreder/OrderServiceImpl.java
+++ src/main/java/hello/core/oreder/OrderServiceImpl.java
@@ -3,10 +3,12 @@ package hello.core.oreder;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
@@ -32,11 +34,12 @@ public class OrderServiceImpl implements OrderService {
 //        this.memberRepository = memberRepository;
 //        this.discountPolicy = discountPolicy;
 //    }
-    @Autowired          //생성자 주입
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+
+//    @Autowired         //생성자 주입 (@RequiredArgsConstructor 으로 대체 가능 해짐)
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order creteOrder(Long memberId, String itemName, int itemPrice) {
