19ffd096d20bd622ccdca5ead032309c5a69904d
ChanUi <hcu55@naver.com>
2024-09-01 16:03:26 +0900

    :sparkles: 조회한 빈이 모두 필요할 때, List, Map

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -5,11 +5,11 @@ import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 //@Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy{
 
-    private int discountFixAmount = 1000; //1000원 할
+    private int discountFixAmount = 1000; //1000원 할인
 
     @Override
     public int discount(Member member, int price) {
